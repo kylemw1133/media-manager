cf6ed0c7ed428a7f991c9479c06acebdb4d7bbb2
Sichan Yuh <sichan.yuh@gmail.com>
2022-03-31 20:52:22 -0400

    delete method for inventory item

--- src/entities/InventoryItem.java
+++ src/entities/InventoryItem.java
@@ -13,7 +13,8 @@ public class InventoryItem {
 
 	private final static String insertInventoryItemSQL = "INSERT INTO INVENTORY_ITEM VALUES (?, ?, ?, ?);";
 	private final static String editInventoryItemSQL = " UPDATE INVENTORY_ITEM SET Quantity=?, Format=?, Location=? WHERE Inventory_ID=?";
-	
+	private final static String deleteInventoryItemSQL = "DELETE FROM INVENTORY_ITEM WHERE Inventory_ID = ?";
+
 	public static int insert(Connection conn, Scanner s) throws SQLException {
 		int id = 0;
 		int i = 1;
@@ -23,19 +24,19 @@ public class InventoryItem {
 		for (TypedAttribute a : colSet) {
 			a.promptForValue(s);
 			a.fillInStmt(insertInventoryItemStmt, i);
-			
+
 			if (a.name.equals("Inventory_ID")) {
 				id = (int) a.value;
 			}
 
 			i++;
 		}
-		
+
 		insertInventoryItemStmt.executeUpdate();
 
 		return id;
 	}
-	
+
 	public static void edit(Connection conn, Scanner s) throws SQLException {
 		LinkedList<TypedAttribute> colSet = Utils.getColumns(conn, "MOVIE");
 		PreparedStatement editInventoryItemStmt = conn.prepareStatement(editInventoryItemSQL);
@@ -51,9 +52,25 @@ public class InventoryItem {
 				a.fillInStmt(editInventoryItemStmt, i++);
 			}
 		}
-		
+
 		editInventoryItemStmt.setInt(i, id);
-		
+
 		editInventoryItemStmt.executeUpdate();
+	}
+
+	public static void delete(Connection conn, Scanner s) throws SQLException {
+		System.out.println("Enter the inventory ID of the item you wish to delete:");
+		PreparedStatement deleteInventoryItemStmt = conn.prepareStatement(deleteInventoryItemSQL);
+
+		String id = "";
+		// repeat promp for inventory id until user inputs a valid integer
+		while (id.equals("") || !id.matches("\\-?\\d+")) {
+			id = s.nextLine();
+		}
+
+		deleteInventoryItemStmt.setString(1, id);
+		deleteInventoryItemStmt.executeQuery();
+
+		deleteInventoryItemStmt.close();
 	}
 }
