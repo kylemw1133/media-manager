6f67310ea469255790b1b5318eb0ef91f753eebe
Sam Amann <amann.29@buckeyemail.osu.edu>
2022-03-31 23:08:03 -0400

    Fixed merge conflicts

--- src/commands/SearchCommand.java
+++ src/commands/SearchCommand.java
@@ -4,14 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-<<<<<<< HEAD
 import entities.*;
-=======
-import entities.Album;
-import entities.Audiobook;
-import entities.InventoryItem;
-import entities.Movie;
->>>>>>> branch 'master' of https://github.com/kylemw1133/media-manager.git
 
 public class SearchCommand {
 
@@ -26,7 +19,6 @@ public class SearchCommand {
 		case "movie":
 			Movie.search(conn, s);
 			break;
-<<<<<<< HEAD
 		case "audiobook":
 			Audiobook.search(conn, s);
 			break;
@@ -35,17 +27,6 @@ public class SearchCommand {
 			break;
 		case "tvshow":
 			TVShow.retrieve(conn, s);
-=======
-		case "tvshow":
-			//TVShow.search(conn, s);
-			break;
-		case "audiobook":
-			Audiobook.search(conn, s);
-			break;
-		case "inventory_item":
-			//InventoryItem.search(conn, s);
->>>>>>> branch 'master' of https://github.com/kylemw1133/media-manager.git
-			break;
 		default:
 			System.out.println("Invalid item type");
 		}
